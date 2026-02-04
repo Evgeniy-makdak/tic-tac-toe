6ba7dd0609037ccefb6634417b9e5893c86ec172
Evgeniy-makdak <makdak23@mail.ru>
2026-02-04 10:05:24 +0300

    Чередование крестиков и ноликов

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,1 +1,17 @@
 const cells = document.querySelectorAll('#field td');
+start(cells);
+
+function start(cells) {
+    let i = 0;
+
+    for (let cell of cells) {
+        cell.addEventListener('click', function () {
+            if (i % 2 === 0) {
+                cell.textContent = 'X';
+            } else {
+                cell.textContent = 'O';
+            }
+            i++;
+        });
+    }
+}
