11443389a4b84ba8acf57fdb37775de1fa084122
Evgeniy-makdak <makdak23@mail.ru>
2026-02-05 16:19:11 +0300

    Добавлено ограничение на повторный клик по ячейке

--- ticTacToe.js
+++ ticTacToe.js
@@ -5,13 +5,13 @@ function start(cells) {
     let i = 0;
 
     for (let cell of cells) {
-        cell.addEventListener('click', function () {
-            if (i % 2 === 0) {
-                cell.textContent = 'X';
-            } else {
-                cell.textContent = 'O';
-            }
-            i++;
-        });
+            cell.addEventListener('click', function () {
+                if (i % 2 === 0 && !cell.textContent) {
+                    cell.textContent = 'X';
+                } else if (i % 2 !== 0 && !cell.textContent) {
+                    cell.textContent = 'O';
+                }
+                i++;
+            }, { once: true });
     }
 }
